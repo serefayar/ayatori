65dc36241b9560cbcdc84b8504bc48222c97e891
Seref R. Ayar <serefayar@gmail.com>
2026-04-05 14:16:58 +0300

    memory strategy and sliding window

--- src/ayatori/memory.clj
+++ src/ayatori/memory.clj
@@ -5,22 +5,52 @@
   (get-messages [this] "Returns the current message list.")
   (clear! [this] "Clears all messages. Returns this."))
 
-(defrecord SlidingWindowMemory [messages max-messages]
+(defn- sliding-window-strategy
+  "Keeps last N messages. Preserves system role messages when :preserve-system true."
+  [messages _msg {:keys [max-messages preserve-system] :or {max-messages 50}}]
+  (if preserve-system
+    (let [system-msgs (filterv #(= :system (:role %)) messages)
+          other-msgs (filterv #(not= :system (:role %)) messages)
+          keep-count (- max-messages (count system-msgs))
+          kept (vec (take-last keep-count other-msgs))]
+      (into system-msgs kept))
+    (vec (take-last max-messages messages))))
+
+(def ^:private strategies
+  {:sliding-window sliding-window-strategy})
+
+(defn- resolve-strategy [strategy-config]
+  (let [strategy-fn (get strategies (:type strategy-config))]
+    (when-not strategy-fn
+      (throw (ex-info "Unknown strategy" {:type (:type strategy-config)})))
+    strategy-fn))
+
+(defrecord Memory [messages strategies-config]
   IMemory
   (add-message [this msg]
-    (swap! messages #(vec (take-last max-messages (conj % msg))))
+    (swap! messages
+           (fn [msgs]
+             (reduce (fn [m cfg]
+                       (let [strategy-fn (resolve-strategy cfg)]
+                         (strategy-fn m msg cfg)))
+                     (conj msgs msg)
+                     strategies-config)))
     this)
   (get-messages [_] @messages)
   (clear! [this] (reset! messages []) this))
 
 (def MemoryConfig
   [:map
-   [:type [:enum :sliding-window]]
-   [:max-messages {:optional true} :int]])
+   [:strategies {:optional true}
+    [:vector [:map
+              [:type :keyword]]]]])
+
+(def default-strategies
+  [{:type :sliding-window :max-messages 50 :preserve-system true}])
 
 (defn make-memory
-  {:malli/schema [:=> [:cat MemoryConfig] :any]}
-  [{:keys [type max-messages] :or {max-messages 50}}]
-  (case type
-    :sliding-window (->SlidingWindowMemory (atom []) max-messages)
-    (throw (ex-info "Unknown memory type" {:type type}))))
+  "Creates a Memory instance with specified strategies."
+  ([] (make-memory {}))
+  ([{:keys [strategies]}]
+   (let [strategies (or strategies default-strategies)]
+     (->Memory (atom []) strategies))))
